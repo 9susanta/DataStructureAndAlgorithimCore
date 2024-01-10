dc150a07db490a07f7170a6c037d48fe12f55c1f
susanta rout <rsusanta0@gmail.com>
2024-01-10 23:35:29 +0530

    Add project files.

---
+++ BinarySearch/BinarySearchAlgo.cs
@@ -0,0 +1,32 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearchAlgo
+    {
+        public static int BinarySearchOperation(int[] list,int item)
+        {
+            int low=0, high=list.Length-1;
+            int mid = 0;
+            int guess = 0;
+            while (low<=high)
+            {
+                mid=(low+high)/2;
+                guess = list[mid];
+
+                if (guess==item)
+                {
+                    return mid;
+                }
+                else if (guess<item)
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+
+            }
+            return -1;
+        }
+    }
+}
---
+++ DataStructureAndAlgorithimCore/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructureAndAlgorithimCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorith Practice!");
+            for (int i = 0; i < 12; i++)
+            {
+                int k = BinarySearch.BinarySearchAlgo.BinarySearchOperation([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11], i);
+
+                Console.WriteLine(k);
+            }
+            
+        }
+    }
+}
