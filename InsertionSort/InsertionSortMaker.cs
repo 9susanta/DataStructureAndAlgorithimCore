fd46217d23967e71bac8fe6eea7d2312bee02a3a
9susanta <rsusanta0@gmail.com>
2024-01-30 21:30:25 +0530

    Rectifited

--- InsertionSort/InsertionSortMaker.cs
+++ InsertionSort/InsertionSortMaker.cs
@@ -4,12 +4,12 @@
     {
         public static int[] GetInsertionSort(int[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 int key = arr[i];
                 int j = i - 1;
 
-                while (j >0 && arr[j]>key)
+                while (j >=0 && arr[j]>key)
                 {
                     arr[j+1] = arr[j];
                     j = j - 1;
